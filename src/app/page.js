1cc60bedef8f44b1b9087edc01c82f3f694f5d2c
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-03-21 19:52:41 +0000

    fixed problem with pictures

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,21 @@
 "use client"
 
-import { useState } from "react";
-import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
-// import Hero from "@/components/Hero";
 
 export default function Home() {
-  const [activeSection, setActiveSection] = useState("hero");
-
   return (
-    <div className="portfolio-container">
-      {/* {activeSection === "hero" && <Hero />} */}
-      {activeSection === "about" && <About />}
-      {activeSection === "projects" && <Projects />}
-      {activeSection === "skills" && <Skills />}
-      {activeSection === "contact" && <Contact />}
-    </div>
+    <>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+    </>
   );
 }
