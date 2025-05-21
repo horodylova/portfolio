137dabac10ef5a64dfe558078bfe7f15228fcb29
Svitlana Horodylova <108434503+horodylova@users.noreply.github.com>
2025-05-21 20:25:18 +0100

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,11 +8,9 @@ export default function About() {
         <div className={styles.summaryContent}>
           <h2 className={styles.summaryTitle}>Summary</h2>
           <p className={styles.summaryText}>
-            <span className={styles.highlightText}>Solving business challenges</span> through AI integrations and Full Stack development (JavaScript,
-            React, Node.js). <span className={styles.flickerText}>Creating custom AI agents</span> and optimising OpenAI API interactions.
+            <span className={styles.highlightText}>Solving business challenges</span> through AI integrations and Full Stack development and strategic content solutions. <span className={styles.flickerText}>Creating scalable application architectures </span> and intuitive user interfaces.
             <br /><br />
-            Transforming complex technical concepts into accessible content with 20 years of editing and
-            team management experience. <span className={styles.highlightText}>European journalism programme laureate</span>. 
+            Transforming complex technical concepts into accessible digital experiences with 20 years of development and content expertise. <span className={styles.highlightText}>European journalism programme laureate</span>. 
             <br /><br />
             Helping companies successfully implement AI and deliver <span className={styles.flickerText}>exceptional user experience</span>.
           </p>
